fc58a1da427338080c165a4690808eef72840bc3
Rishabh7777 <rishabh.tiwari_cs18@gla.ac.in>
2020-12-29 14:17:03 +0530

    romved redundant ;

--- src/Scroll.js
+++ src/Scroll.js
@@ -2,7 +2,7 @@ const Scroll = (props) => {
     return (
         <div style={{overflow: 'scroll', height: '440px'}}>
             {/* every react component has props, state and children */}
-            {props.children};
+            {props.children}
         </div>
     )
 }
